cec7e0d41601dca57aa41308c4ea8432c3885120
Dan Mosora <dan@stitchdata.com>
2019-02-07 16:19:22 +0000

    Version 1.0.1 and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.0.1
+  * Rename `a_workspaces` to `workspaces` [#4](https://github.com/singer-io/tap-toggl/pull/4)
+
 ## 1.0.0
   * General release of the tap
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-toggl',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from the Toggl API',
       author='Stitch',
       url='http://github.com/singer-io/tap-toggl',
